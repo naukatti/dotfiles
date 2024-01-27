2dae6a0c0bd1884d901634196306ada861b28271
naukatti <noora.peltok@gmail.com>
2024-01-28 01:32:46 +0200

    add first configs

---
+++ homebrew.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+brew install \
+bat \
+cmatrix \
+dust \
+eza \
+fnm \
+git \
+htop \
+kitty \
+pipes-sh \
+ripgrep \
+tokei \
+topgrade \
+tree \
+tty-clock \
+zoxide \

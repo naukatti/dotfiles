72dc7fb648b5655d9969077b259db26e4e9d14bf
naukatti <noora.peltok@gmail.com>
2024-01-28 16:45:45 +0200

    add more quality of life improvements

--- homebrew.sh
+++ homebrew.sh
@@ -3,17 +3,23 @@ set -euo pipefail
 
 brew install \
 bat \
+bottom \
 cmatrix \
 dust \
 eza \
+fd \
 fnm \
+font-iosevka \
 git \
+hexyl \
 htop \
 kitty \
 pipes-sh \
 ripgrep \
+tealdeer \
 tokei \
 topgrade \
 tree \
 tty-clock \
+viu \
 zoxide \
